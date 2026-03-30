3458866f54377b16efed8aa02b35305c26b1b66e
Alexandre Costa <alexandre.costa@finaya.tech>
2026-03-30 13:33:50 -0300

    Revert "Refactor branch conditionals in entrypoint.sh for improved version handling"
    This reverts commit d891f32e7588b1c00407e3ebee06110b9c08405f.

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,9 +6,8 @@ echo ${GITHUB_REF#refs/*/}
 
 REF=${GITHUB_REF#refs/heads/}
 COMMIT_ID=${GITHUB_SHA::7}
-SEMVER_REGEX='^(0|[1-9][0-9]*)\.(0|[1-9][0-9]*)\.(0|[1-9][0-9]*)(-((0|[1-9][0-9]*|[0-9]*[a-zA-Z-][0-9a-zA-Z-]*)(\.(0|[1-9][0-9]*|[0-9]*[a-zA-Z-][0-9a-zA-Z-]*))*))?(\+([0-9a-zA-Z-]+(\.[0-9a-zA-Z-]+)*))?$'
 
-if [[ "$REF" == "develop" || ( "$REF" != *"-rc_"* && "$REF" != *"-rc"* && ! ${GITHUB_REF#refs/*/} =~ $SEMVER_REGEX ) ]]; then
+if [[ "$REF" == "develop" || "$REF" == "develop-spring-2" ]]; then
     printf "Branch name: $REF \nCommit ID: $COMMIT_ID"
     echo "RELEASE_VERSION=$COMMIT_ID" >> $GITHUB_ENV
     echo "GITOPS_BRANCH=develop" >> $GITHUB_ENV
@@ -23,7 +22,7 @@ elif [[ "$REF" == *"-rc"* ]]; then
     echo "RELEASE_VERSION=${GITHUB_REF#refs/*/}" >> $GITHUB_ENV
     echo "GITOPS_BRANCH=homolog" >> $GITHUB_ENV
 
-elif [[ ${GITHUB_REF#refs/*/} =~ $SEMVER_REGEX ]]; then
+elif [[ ${GITHUB_REF#refs/*/} =~ ^(0|[1-9][0-9]*)\.(0|[1-9][0-9]*)\.(0|[1-9][0-9]*)(-((0|[1-9][0-9]*|[0-9]*[a-zA-Z-][0-9a-zA-Z-]*)(\.(0|[1-9][0-9]*|[0-9]*[a-zA-Z-][0-9a-zA-Z-]*))*))?(\+([0-9a-zA-Z-]+(\.[0-9a-zA-Z-]+)*))?$ ]]; then
     printf "New tag: ${GITHUB_REF#refs/*/}"
     echo "RELEASE_VERSION=${GITHUB_REF#refs/*/}" >> $GITHUB_ENV
     echo "GITOPS_BRANCH=release" >> $GITHUB_ENV
